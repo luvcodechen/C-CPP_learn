b5ba1de5867a062181d55a68135663016ec61c37
luvcodechen <chen_ney@qq.com>
2022-07-20 17:43:28 +0800

    test10

---
+++ 树和二叉树练习/test10/test.c
@@ -0,0 +1,173 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#define Isletter(a) ((((a)>='a')&&((a)<='z'))||(((a)>='A')&&((a)<='Z')))
+#define  MAXNUM(a,b) ((a)>(b)?(a):(b))
+//设一棵二叉树中各节点值均不同，给定两个数组存先中序遍历，据此建立该数的二叉链表
+#define MAX 20
+typedef char ElemType;
+//二叉链表结构存储二叉树
+typedef struct TreeNode
+{
+	ElemType data;
+	struct TreeNode* left, * right;
+}BNode, * BiTree;
+
+//初始化
+void InitTree(BiTree* T)
+{
+	*T = NULL;
+}
+//先序建树
+bool preCreatTree(BiTree* T)
+{
+	ElemType ch;
+	ch = getchar();
+	if (ch == ' ')
+		*T = NULL;
+	else {
+		if (!(*T = (BNode*)calloc(1, sizeof(BNode))))
+			return false;
+		(*T)->data = ch;
+		preCreatTree(&(*T)->left);
+		preCreatTree(&(*T)->right);
+	}
+	return true;
+}
+//先序遍历
+void preorderTraverse(BiTree T)
+{
+	if (T)
+	{
+		putchar(T->data);
+		preorderTraverse(T->left);
+		preorderTraverse(T->right);
+	}
+}
+//中序遍历二叉树
+void InorderTraverse(BiTree T)
+{
+	if (T)
+	{
+		InorderTraverse(T->left);
+		putchar(T->data);
+		InorderTraverse(T->right);
+	}
+}
+//后序遍历二叉树
+void postorderTraverse(BiTree T)
+{
+	if (T)
+	{
+		postorderTraverse(T->left);
+		postorderTraverse(T->right);
+		putchar(T->data);
+	}
+}
+//二叉树高度
+int  DepthTree(BiTree T)
+{
+	return T == NULL ? 0 : MAXNUM(DepthTree(T->left), DepthTree(T->right)) + 1;
+}
+//层序建树（此处使用顺序队方便操作，会造成空间浪费）
+void levelCreat(BiTree* T)
+{
+	BiTree Q[MAX] = { NULL };//辅助队列
+	int front, rear;//队头队尾指针
+	front = rear = 0;
+	ElemType c;
+	BiTree new = NULL;
+	while (scanf("%c", &c) != EOF)
+	{
+		if ('\n' == c)
+			break;
+		new = (BNode*)calloc(1, sizeof(BNode));//新结点
+		if (new)
+			new->data = c;
+		if (*T == NULL)//若树根为空，第一个输入的结点值作为树根
+		{
+			*T = new;
+			Q[rear++] = new;//入队
+			continue;
+		}
+		if (Q[front]->left == NULL)//新节点从队头的左孩子找空位插入
+			Q[front]->left = new;
+		else if (Q[front]->right == NULL)//左孩子不空找右孩子
+			Q[front]->right = new;
+		else//找下一个结点，队头指针后移
+			Q[++front]->left = new;
+		Q[rear++] = new;//新节点入队
+	}
+}
+//普通层次遍历
+void leveltravese(BiTree T)
+{
+	if (!T)
+		return;
+	BiTree Q[MAX] = { NULL };//需要辅助队列
+	int front, rear;
+	front = rear = 0;
+	Q[rear++] = T;
+	BNode* p;//临时存结点
+	while (front < rear)
+	{
+		p = Q[front++];
+		printf("%c", p->data);
+		if (p->left)
+			Q[rear++] = p->left;
+		if (p->right)
+			Q[rear++] = p->right;
+	}
+}
+//销毁二叉树
+void DestroyBiTree(BiTree* T)
+{
+	if (*T)
+	{
+		DestroyBiTree(&(*T)->left); /* 销毁左孩子子树 */
+		DestroyBiTree(&(*T)->right); /* 销毁右孩子子树 */
+		free(*T); /* 释放根结点 */
+		*T = NULL; /* 空指针赋0 */
+	}
+}
+//根据先序和中序序列建树,A、B分别存储前序和中序序列，pre、mid分别是前序和中序开始遍历的位置，n为树的结点个数
+BiTree pre_ordercreate(ElemType *A,ElemType *B,int pre,int mid,int n)
+{
+	if (n == 0)
+		return NULL;
+	BiTree p= (BNode*)calloc(1, sizeof(BNode));
+	if (!p)
+		return;
+	p->data = A[pre];
+	int i = 0;
+	for (i = 0; B[mid+i] != A[pre]; i++);//根节点在中序遍历中的位置
+	p->left = pre_ordercreate(A, B, pre + 1, mid, i);//递归建立左子树
+	p->right = pre_ordercreate(A, B, pre + i + 1, mid + i + 1, n - i - 1);//递归建立右子树
+	return p;
+
+}
+int main()
+{
+	BiTree T;
+	InitTree(&T);
+	//preCreatTree(&T);//先序建树
+	//levelCreat(&T);//层序建树
+	ElemType A[MAX]="abdhpqirsejkcflmgno";//先序序列
+	ElemType B[MAX]="phqdrisbjekalfmcngo";//中序序列
+	int len = strlen(A);
+	T=pre_ordercreate(A,B,0,0,len);//根据先中序序列建树
+	printf("\n先序遍历：");
+	preorderTraverse(T);
+	printf("\n中序遍历：");
+	InorderTraverse(T);
+	printf("\n后序遍历：");
+	postorderTraverse(T);
+	printf("\n层序遍历：");
+	leveltravese(T);
+	printf("\n二叉树高度:");
+	printf("%d", DepthTree(T));
+	printf("\n");
+	return 0;
+}
